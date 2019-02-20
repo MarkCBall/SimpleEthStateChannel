6526866f1045b057f6f1f61b142f585bae5f6476
MarkCBall <moball@hotmail.com>
2019-02-20 17:04:14 -0500

    submit new tx working

--- ExpressBackend/routes/Transaction.js
+++ ExpressBackend/routes/Transaction.js
@@ -1,34 +1,82 @@
 var express = require('express');
 var router = express.Router();
 
-
-
 var level = require('level')
-var db = level('./dbb', {valueEncoding: 'json'})
+var db = level('./dbTransactions', {valueEncoding: 'json'})
+
+router.post('/new', async function(req, res, next) {
+    //EXAMPLE req.header {CID:5}
+    //EXAMPLE req.body
+    // {    sig1: ""   OR sig2: ""
+    //      u1Bal: 2
+    //      u2Bal: 35
+    // }
+
+    //verify the signature is valid
+    //nonce = highestNonce++ at Hash(CID)
+    var highestNonce = 0;
+    var CID = req.headers.cid;
+    //console.log(CID)
+    highestNonce = await db.get("HighestNonce"+CID)
+    .catch(() => {return 0})
+ 
+    var data = req.body;
+    console.log("data is", data)
+    var nextNonce = highestNonce+1;
+    console.log("now using nonce # ",nextNonce )
+
+    db.put("HighestNonce"+CID,nextNonce)
+    .catch(console.log)
+
+    //create new data entry at at Hash(CID,Nonce+1) 
+    db.put(CID+""+nextNonce,data)
+    .catch(console.log)
+    res.render('index', { title: 'Post completed correctly' });//clean this line later
+});
 
 
-POST@(/proposeNewTx/:CID)
-	//req.body includes one signature and both new balances
-//verify the signature is valid
-//nonce = highestNonce++ at Hash(CID) 
-//create new data entry at at Hash(CID,Nonce+1) 
-	u1NewBalance
-	u2NewBalance
-	either sig1 or sig2
+router.get('/HighestNonce', async function(req, res, next) {
+    //EXAMPLE req.header {CID:5}
+    var CID = req.headers.cid;
+    db.get("HighestNonce"+CID)
+    .then((dbres) => {
+        res.send(JSON.stringify(dbres))
+        //console.log(dbres)
+     })
+    .catch((err) => {
+        res.send(JSON.stringify(0))
+        //console.log(err)
+    })
+});
 
-// POST@(/confirmTx/:CID/:nonce)
-// 	//req.body includes sig
-// //verify sig is the required countersignature
-// //update the signature in the database
-// //highestSignedNonce = highestNonce at at Hash(CID) 
+router.get('/getTx', async function(req, res, next) {
+    //EXAMPLE req.header {CID:5, nonce:2}
+    var CID = req.headers.cid;
+    var nonce = req.headers.nonce;
+    console.log(CID,"xx",nonce)
+    db.get(CID+""+nonce)
+    .then((dbres) => {
+        res.send(JSON.stringify(dbres))
+        console.log("dbres is", dbres)
+     })
+    .catch((err) => {
+        res.send(JSON.stringify(0))
+        console.log("error is",err)
+    })
+});
 
 
-// GET@(/getTx/:CID/:nonce)
-// //returns the Tx details
-// GET@(/getHighestNonce/:CID)
-// //returns the highest nonce on the specified channel
-// GET@(/getHighestSignedNonce/:CID)
-// //returns the highest fully signed nonce on the specified channel
+
+// // POST@(/confirmTx/:CID/:nonce)
+// // 	//req.body includes sig
+// // //verify sig is the required countersignature
+// // //update the signature in the database
+// // //highestSignedNonce = highestNonce at at Hash(CID) 
+
+
+
+// // GET@(/getHighestSignedNonce/:CID)
+// // //returns the highest fully signed nonce on the specified channel
 
 
 
--- frontend/src/components/ProposeNewTx.js
+++ frontend/src/components/ProposeNewTx.js
@@ -9,59 +9,87 @@ class ProposeNewTx extends Component {
         super(props);
         this.state = {
             MyTokenTx: 0,
-            YourTokenTx:0
+            CounterpartyTokenTx:0
         }
     }
     handleMyTokenChange = (event) => {
         this.setState({MyTokenTx:event.target.value})
     }
+    handleCounterpartyTokenChange = (event) => {
+        this.setState({CounterpartyTokenTx:event.target.value})
+    }
 
-    MySubmitAndSign = () => {
+    userOneIsMe = () => {
+        return this.props.u1Address === this.props.addressSignedIn
+    }
+
+    // getMyTokenName = () => {
+    //     return userOneIsMe() ? this.props.u1TokenName : this.props.u2TokenName
+
+    // }
+    // getCounterpartyTokenName = () => {
+    //     if (this.props.u1Address === this.props.addressSignedIn)
+    //         return this.props.u2TokenName
+    //     return this.props.u1TokenName
+    // }
+
+    SubmitAndSign = () => {
+        var body;
+        if (this.userOneIsMe()){
+            body = {
+                sig1:"put functioncall here",
+                u1Bal:this.state.MyTokenTx,
+                u2Bal:this.state.CounterpartyTokenTx
+            }
+        }
+        else{
+            body = {
+                sig2:"put functioncall here",
+                u1Bal:this.state.CounterpartyTokenTx,
+                u2Bal:this.state.MyTokenTx
+            }
+        }
+        //console.log("body is",body)
+        fetch("http://localhost:3001/Transaction/new", {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8",
+                    "cid":this.props.activeChannel,
+                },
+                body: JSON.stringify(body)
+            })
+            .then("success",console.log)
+            .catch("failure",console.log)
+
         //call to the database
     }
 
     render() {
         return (
             <div>
-                {/* Next Nonce: -13- <br/>
 
-                 <div className="container-full">
-                    <div className="row">
-                        <div className="col-3 col-solid"></div>
-                        <div className="col-2 col-solid">
-                            You
-                        </div>
-                        <div className="col-2 col-solid">
-                            CounterParty
-                        </div>
-                    </div>
-
-                   
-                    <TxTokenRow 
-                        tokenName={"Marks"}
-                        total={50}
-                        yourBal={37}
-                    />
-
-                    <TxTokenRow 
-                        tokenName={"Matts"}
-                        total={30}
-                        yourBal={6}
-                    /> */}
+                {/* <button onClick={()=>console.log(this.props.u1TokenName)}>clickme</button> */}
+                
                     
                     You are trading 
                     <input 
                         type="text" 
-                        size="3"
+                        size="2"
                         onChange={ this.handleMyTokenChange   }
                         value={this.state.MyTokenTx}
-                    />
-                    {"Mark"}
-                    tokens for
-                    {"xxx"}
-                    {"Matt"}
-                    tokens 
-                    <button> submit and sign</button>
+                    /> &nbsp;
+                    {this.userOneIsMe() ? this.props.u1TokenName : this.props.u2TokenName}
+                    &nbsp; tokens for &nbsp;
+                    <input 
+                        type="text" 
+                        size="2"
+                        onChange={ this.handleCounterpartyTokenChange   }
+                        value={this.state.CounterpartyTokenTx}
+                    />&nbsp;
+                    {this.userOneIsMe() ? this.props.u2TokenName : this.props.u1TokenName}
+                    &nbsp; tokens &nbsp;
+                    <button onClick={() => this.SubmitAndSign()}> submit and sign</button>
                     <br/>
 
 
@@ -79,11 +107,43 @@ class ProposeNewTx extends Component {
 
 function mapStateToProps(state) {
     return {
-        activeChannel: state.InteractReduxState.activeChannel
+        activeChannel: state.InteractReduxState.activeChannel,
+        addressSignedIn: state.InteractReduxState.addressSignedIn,
+        u1TokenName: state.InteractDatabase.ActiveChannelDetails.u1TokenName,
+        u2TokenName: state.InteractDatabase.ActiveChannelDetails.u2TokenName,
+        u1Address : state.InteractDatabase.ActiveChannelDetails.u1Address
         //do stuff here to pull in token names for the channel specified
     }
 }
 export default connect(mapStateToProps)(ProposeNewTx);
 
 //export default ProposeNewCh;
+
+
+
+// {/* Next Nonce: -13- <br/>
+
+//                  <div className="container-full">
+//                     <div className="row">
+//                         <div className="col-3 col-solid"></div>
+//                         <div className="col-2 col-solid">
+//                             You
+//                         </div>
+//                         <div className="col-2 col-solid">
+//                             CounterParty
+//                         </div>
+//                     </div>
+
+                   
+//                     <TxTokenRow 
+//                         tokenName={"Marks"}
+//                         total={50}
+//                         yourBal={37}
+//                     />
+
+//                     <TxTokenRow 
+//                         tokenName={"Matts"}
+//                         total={30}
+//                         yourBal={6}
+//                     /> */}
 
