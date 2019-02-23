c21e328c7c1042c7f2018afe8e2224cabc5e9911
MarkCBall <moball@hotmail.com>
2019-02-23 09:32:38 -0500

    interact with ropsten contract

--- frontend/src/redux/actions/InteractBlockchain.js
+++ frontend/src/redux/actions/InteractBlockchain.js
@@ -1,6 +1,14 @@
 
 
+const ethers = require('ethers')
+//init ethers
+//let provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
+let provider = ethers.getDefaultProvider('ropsten');
 
+//get contract info
+let StateChannelJson = require('../../SolidityJSON/StateChannels.json')
+let StateChannelAbi = StateChannelJson.abi;
+let StateChannelBytecode = StateChannelJson.bytecode
 
 
     
@@ -11,30 +19,21 @@ export default {
     countersignChannel:(dispatch) => {
         return async (dispatch,getState) => {
 
-            const ethers = require('ethers')
 
 
-            let deployedContract
 
 
-            //init ethers
-            let provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
-
-            //get contract info
-            let StateChannelJson = require('../../SolidityJSON/StateChannels.json')
-            let StateChannelAbi = StateChannelJson.abi;
-            let StateChannelBytecode = StateChannelJson.bytecode
 
             //get wallets --> do this through metamask later?
-            let firstwallet = new ethers.Wallet(getState().InteractReduxState.privKey).connect(provider)
+            let activeWallet = new ethers.Wallet(getState().InteractReduxState.privKey).connect(provider)
 
 
 
             //get a contract --> change this to existing contract later
-            let ContractFactory = await new ethers.ContractFactory(StateChannelAbi, StateChannelBytecode).connect(firstwallet);
-            deployedContract = await ContractFactory.deploy()
-            //let deployedaddress = "0xE802cA7b7D9F3b9df1CB1f772444cFe2dC3C7A47"
-            //let deployedContract = new ethers.Contract(deployedaddress,StateChannelAbi,provider).connect(secondwallet);
+            // let ContractFactory = await new ethers.ContractFactory(StateChannelAbi, StateChannelBytecode).connect(activeWallet);
+            // let deployedContract = await ContractFactory.deploy()
+            let deployedaddress = "0x7034cC3eeDf27AC3f96599F0dD0de74A7960285B"
+            let deployedContract = new ethers.Contract(deployedaddress,StateChannelAbi,provider).connect(activeWallet);
             //console.log("deployed contract is ", deployedContract)
 
 
@@ -50,7 +49,7 @@ export default {
                 //CID FROM ACD
 
 
-                let u1Address = ACD.u1Address//firstwallet.signingKey.address //address
+                let u1Address = ACD.u1Address//activeWallet.signingKey.address //address
                 //let u2Address = ACD.u2Address//secondwallet.signingKey.address
                 let u1TokenName = ACD.u1TokenName//"Marks" //string memory
                 let u2TokenName = ACD.u2TokenName //"Matts" //string memory
@@ -64,7 +63,7 @@ export default {
                 
 
                 //console.log(u1Address)
-                //console.log(firstwallet.signingKey.address)
+                //console.log(activeWallet.signingKey.address)
                 //console.log(ACD)
                 //console.log("deployedcontract ")
 
@@ -74,6 +73,9 @@ export default {
                     v1, r1, s1, CID, u1Address, u1TokenName, u2TokenName, u1InitialTokenBal, u2InitialTokenBal
                 ).then((x) => console.log("\n\nthen", x))
                 .catch((err) => console.log("\n\ncatch", err))
+
+
+                //remove requested from database
 
         }
 
