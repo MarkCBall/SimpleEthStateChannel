fc9cc01c9509a0028363b0ca982ed898b0d465b2
MarkCBall <moball@hotmail.com>
2019-03-07 15:21:37 -0500

    submit tx working, need to re-impliment countersign tx

--- frontend/src/components/ChDisplays/ChOngoing.js
+++ frontend/src/components/ChDisplays/ChOngoing.js
@@ -2,21 +2,49 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import InteractBlockchain from "../../redux/actions/InteractBlockchain";
 
-import CurrentBalances from "../ContractInfo/CurrentBalances";
-import InitialBalances from "../ContractInfo/InitialBalances";
-import CountersignTx from "../CountersignTx";
-import ProposeNewTx from "../ProposeNewTx";
+// import CurrentBalances from "../ContractInfo/CurrentBalances";
+// import InitialBalances from "../ContractInfo/InitialBalances";
+// import CountersignTx from "../CountersignTx";
+//import ProposeNewTx from "../ProposeNewTx";
 
 //relative imports
 import "./ChOngoing.css";
+
+//make this an import???
+const ethers = require('ethers')
+
+
 
 class ChOngoing extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          ToggleDispNewTx: false
+          ToggleDispNewTx: false,
+          u1Bal:0,
+          u2Bal:0
         }
     }
+
+    handleU1Change = (event) => {
+        let u1Bal = Number(event.target.value)
+        if ((u1Bal >=0) && (u1Bal <= Number(this.props.u1InitialTokenBal))){
+            this.setState({
+                ...this.state,
+                u1Bal:u1Bal
+            })
+        }
+        
+    }
+    handleU2Change = (event) => {
+        let u2Bal = Number(event.target.value)
+        if ((u2Bal >=0)  && (u2Bal <= Number(this.props.u2InitialTokenBal))){
+            this.setState({
+                ...this.state,
+                u2Bal:u2Bal
+            })
+        }
+    }
+
 
     toggleDispNewTx = () => {
         this.setState({ToggleDispNewTx:!this.state.ToggleDispNewTx})
@@ -34,8 +62,65 @@ class ChOngoing extends Component {
             Raw Signature
             </button>
         }
-        
     }
+
+
+        //bytes32 TxHash = keccak256(abi.encodePacked(CID,nonce,u1BalRetained,u2BalRetained));
+        signNewTxData = async (u1BalRetained,u2BalRetained) => {
+            //console.log(this.props.activeChannelNum,this.props.HighestNonce,u1BalRetained,u2BalRetained)
+    
+            //generate the hash to sign based on channel details
+            let hashedEncodedChannelData = ethers.utils.solidityKeccak256(
+                ['uint', 'uint', 'uint', 'uint'],
+                [
+                    this.props.activeChannelNum,
+                    this.props.HighestNonce,
+                    u1BalRetained,
+                    u2BalRetained
+                ]
+            );
+            let ArrayifiedHashedEncodedChannelData = ethers.utils.arrayify(hashedEncodedChannelData)
+    
+            let firstwallet = new ethers.Wallet(this.props.privateKey)
+            let flatSig = await firstwallet.signMessage(ArrayifiedHashedEncodedChannelData)//.then(console.log)
+            let sig = ethers.utils.splitSignature(flatSig);
+            //console.log(sig)
+            return sig
+        }
+    
+     
+    
+        SubmitAndSign = async () => {
+            var body;
+            if (this.props.userOneIsMe){
+                body = {
+                    sig1: await this.signNewTxData(this.state.u1Bal,this.state.u2Bal),
+                    u1Bal:this.state.u1Bal,
+                    u2Bal:this.state.u2Bal
+                }
+            }
+            else{
+                body = {
+                    sig2: await this.signNewTxData(this.state.u1Bal,this.state.u2Bal),
+                    u1Bal:this.state.u1Bal,
+                    u2Bal:this.state.u2Bal
+                }
+            }
+            fetch("http://35.183.188.67:3001/Transaction/new", {
+                    method: "POST",
+                    mode: "cors",
+                    headers: {
+                        "Content-Type": "application/json; charset=utf-8",
+                        "cid":this.props.activeChannelNum,
+                    },
+                    body: JSON.stringify(body)
+                })
+                .then("success",console.log)
+                .catch("failure",console.log)
+    
+        }
+
+
     
 
     render() {
@@ -126,53 +211,35 @@ class ChOngoing extends Component {
                     </div>
                 </div>
 
-                <ProposeNewTx/>
-                <br/>
-                Make the stuff below cover this functionality nicely<br/>
-
                 <div className="row">
                     <div className="col-4 col-solid">
                         Make new Tx Offer (Button)
+                        <br/>
+                        <button className="btn btn-success" onClick={() => this.SubmitAndSign()}> submit and sign</button>
                     </div>
                     <div className="col-4 col-solid">
-                        {this.props.u1BalUnconfirmed+" "+this.props.u1TokenName} tokens
+                        <input 
+                            type="text" 
+                            size="2"
+                            onChange={ this.handleU1Change   }
+                            value={this.state.u1Bal}
+                        />
+                        {" "+this.props.u1TokenName} tokens
                         <br/>
-                        {this.props.u2InitialTokenBal-this.props.u2BalUnconfirmed+""+this.props.u2TokenName} tokens
+                        {this.props.u2InitialTokenBal-this.state.u2Bal+""+this.props.u2TokenName} tokens
                     </div>
                     <div className="col-4 col-solid">
-                        {this.props.u1InitialTokenBal-this.props.u1BalUnconfirmed+""+this.props.u1TokenName} tokens
+                        {this.props.u1InitialTokenBal-this.state.u1Bal+""+this.props.u1TokenName} tokens
                         <br/>
-                        {this.props.u2BalUnconfirmed+" "+this.props.u2TokenName} tokens
+                        <input 
+                            type="text" 
+                            size="2"
+                            onChange={ this.handleU2Change   }
+                            value={this.state.u2Bal}
+                        />
+                        {" "+this.props.u2TokenName} tokens
                     </div>
                 </div>
-
-
-                {/* <div className="row line-above">
-                    <div className="col-6 col-solid">
-                    <CurrentBalances/>
-                    </div>
-                    <div className="col-6 col-solid">
-                    <InitialBalances/>
-                    </div>
-                </div>
-
-                <br/><br/>
-
-                <CountersignTx/>
-                
-                
-                
-                
-                <br/><br/>
-
-
-
-                <button className="btn btn-info" onClick={this.toggleDispNewTx}>Toggle Propose New Transaction</button>
-                <br/>
-                {this.state.ToggleDispNewTx && 
-                    <ProposeNewTx/>
-                } */}
-
 
             </div>
         );
@@ -184,6 +251,9 @@ class ChOngoing extends Component {
 function mapStateToProps(state) {
     return {
         state:state,
+
+        activeChannelNum: state.InteractReduxState.activeChannel.channel,
+        privateKey : state.InteractReduxState.privKey,
 
         //userOneIsMe,u1Address, u1InitialTokenBal,u1Sig OR u2Sig, u1TokenName.. same for u2
         ...state.InteractDatabase.ActiveChannelDetails,
@@ -200,11 +270,6 @@ function mapStateToProps(state) {
         u2BalUnconfirmed:state.InteractDatabase.LatestTxDetails.u2Bal,
         sig1Unconfirmed:state.InteractDatabase.LatestTxDetails.sig1,
         sig2Unconfirmed:state.InteractDatabase.LatestTxDetails.sig2,
-
-        //u2Address:
-        // address: state.InteractReduxState.addressSignedIn,
-        // addressIsValid: state.InteractReduxState.addressIsValid,
-        // pendingChannels: state.InteractDatabase.PendingChannels
     }
 }
 
